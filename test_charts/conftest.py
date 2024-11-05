6438c1ff124867f7ad50f8f8deca2948b98d1d1a
humble-wolf <paulfarkas0707@gmail.com>
2024-11-05 15:05:18 +0100

    first commit

---
+++ test_charts/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from test_charts.users.models import User
+from test_charts.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
